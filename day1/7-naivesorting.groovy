e33c044944c8c431992602874d9ab803e4d914b0
Joel Armando <joelarma@gmail.com>
2014-10-19 09:58:43 +0100

    "If" were nested and they needed to be consecutive.

--- day1/7-naivesorting.groovy
+++ day1/7-naivesorting.groovy
@@ -9,16 +9,18 @@ if (num1 >  num2) {
         t = num2
         num2 = num1
         num1 = t
-        if (num2 > num3) {
+}
+        
+if (num2 > num3) {
                 t = num2
                 num2 = num3
                 num3 = t
         // num3 contains the largest number.
-                if (num1 > num2) {
-                        t = num1
-                        num1 = num2
-                        num2 = t
-                }
-        }
+} 
+if (num1 > num2) {
+        t = num1
+        num1 = num2
+        num2 = t
 }
+
 println(num1 + ", " + num2 + ", " + num3);
