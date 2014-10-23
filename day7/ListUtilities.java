b902a1d45e2b988088ceacabf6d71135bd0eccf3
Joel Armando <joelarma@gmail.com>
2014-10-23 13:18:11 +0100

    Add some changes to make the solution more accurate

--- day7/ListUtilities.java
+++ day7/ListUtilities.java
@@ -2,15 +2,20 @@ public class ListUtilities {
 
     private static IntegerList list;
 
-    public static void main(String[] a) {
-	  int[] ls = {1,2,3,4,5,6,7,8,9,10};
+    public static IntegerList arrayToList(int[] ar) {
 	  int i = 0;
 	  list = new IntegerList();
 	  do {
-		list.add(ls[i]);
+		list.add(ar[i]);
 		i++;
-		} while (i <  ls.length);
-	  list.prettyPrint();
-    }
+		} while (i <  ar.length);
+		return list;
+	}
+	
+	public static void main (String[] args){
+    	int[] ls = {1,2,3,4,5,6,7,8,9,10};
+    	list = arrayToList(ls);
+    	list.prettyPrint();
+	}
 }
  
