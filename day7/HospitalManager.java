a2ec44251fcecf25ddef2abf7b660f33cf005d54
Joel Armando <joelarma@gmail.com>
2014-10-27 21:35:10 +0000

    Indentation fixes

--- day7/HospitalManager.java
+++ day7/HospitalManager.java
@@ -6,22 +6,22 @@ class Patient {
 	
 
     public Patient (String name, int age, String illness) {
-	this.name = name;
-	this.age = age;
-	this.illness = illness;
-	this.nextPatient = null;
+		this.name = name;
+		this.age = age;
+		this.illness = illness;
+		this.nextPatient = null;
     }
 
     public Patient getNext() {
-	return nextPatient;
+		return nextPatient;
     }
 
     public void setNext(Patient patient) {
-	this.nextPatient = patient;
+		this.nextPatient = patient;
     }
 
     public String toString() {
-	return this.name + ", " + this.age + ", "+ this.illness;
+		return this.name + ", " + this.age + ", "+ this.illness;
     }
 
 }
@@ -30,49 +30,49 @@ class PatientList {
     Patient head;
 
     public void addPatient(Patient newPatient) {
-	newPatient.setNext(head);
-	head = newPatient;
+		newPatient.setNext(head);
+		head = newPatient;
     }
 
     public boolean delPatient(Patient patient) {
-	if (head == patient) {
-	    head = head.getNext();
-	    patient.setNext(null);
-	    return true;
-	}
-	Patient prev = head;
-	Patient p = head.getNext();
-	while (p != null) {
-	    if (p == patient) {
-		prev.setNext(p.getNext());
-		p.setNext(null);
-		return true;
+		if (head == patient) {
+	    	head = head.getNext();
+	    	patient.setNext(null);
+	    	return true;
 	    }
-	    prev = p;
-	    p = p.getNext();
-	}
-	return false;
+		Patient prev = head;
+		Patient p = head.getNext();
+		while (p != null) {
+	    	if (p == patient) {
+				prev.setNext(p.getNext());
+				p.setNext(null);
+				return true;
+	    	}
+	    	prev = p;
+	    	p = p.getNext();
+		}
+		return false;
     }
 
     public int countPatients() {
-	int i = 0;
-	Patient p = head;
+		int i = 0;
+		Patient p = head;
 
-	while (p != null) {
-	    i++;
-	    p = p.getNext();
-	}
-	return i;
+		while (p != null) {
+	    	i++;
+	    	p = p.getNext();
+		}
+		return i;
     }
 
     public void prettyPrint() {
-	Patient p = head;
+		Patient p = head;
 
-	System.out.print("Patients:\n");
-	while (p != null) {
-	    System.out.print("  " + p + "\n");
-	    p = p.getNext();
-	}
+		System.out.print("Patients:\n");
+		while (p != null) {
+	    	System.out.print("  " + p + "\n");
+	    	p = p.getNext();
+		}
     }
 
 }
@@ -81,31 +81,31 @@ public class HospitalManager {
     static PatientList patients = new PatientList();
 	
     public static void main (String[] args){
-	Patient first = new Patient("Maryann", 76, "Meningitis");
-	patients.addPatient(first);
-	patients.prettyPrint();
-	patients.addPatient(new Patient("Mary", 66, "Meningitis"));
-	patients.prettyPrint();
-	patients.addPatient(new Patient("Lily", 65, "Tuberculosis"));
-	patients.prettyPrint();
-	Patient mid = new Patient("Louis", 26, "Meningitis");
-	patients.addPatient(mid);
-	patients.prettyPrint();
-	patients.addPatient(new Patient("Carlos", 15, "Meningitis"));
-	patients.prettyPrint();
-	patients.addPatient(new Patient("Peter", 36, "Tuberculosis"));
-	patients.prettyPrint();
-	patients.addPatient(new Patient("Ann", 36, "Meningitis"));
-	patients.prettyPrint();
-	Patient last = new Patient("Kelly", 33, "Tuberculosis");
-	patients.addPatient(last);
-	patients.prettyPrint();
-	patients.delPatient(mid);
-	patients.prettyPrint();
-	patients.delPatient(first);
-	patients.prettyPrint();
-	patients.delPatient(last);
-	patients.prettyPrint();
+		Patient first = new Patient("Maryann", 76, "Meningitis");
+		patients.addPatient(first);
+		patients.prettyPrint();
+		patients.addPatient(new Patient("Mary", 66, "Meningitis"));
+		patients.prettyPrint();
+		patients.addPatient(new Patient("Lily", 65, "Tuberculosis"));
+		patients.prettyPrint();
+		Patient mid = new Patient("Louis", 26, "Meningitis");
+		patients.addPatient(mid);
+		patients.prettyPrint();
+		patients.addPatient(new Patient("Carlos", 15, "Meningitis"));
+		patients.prettyPrint();
+		patients.addPatient(new Patient("Peter", 36, "Tuberculosis"));
+		patients.prettyPrint();
+		patients.addPatient(new Patient("Ann", 36, "Meningitis"));
+		patients.prettyPrint();
+		Patient last = new Patient("Kelly", 33, "Tuberculosis");
+		patients.addPatient(last);
+		patients.prettyPrint();
+		patients.delPatient(mid);
+		patients.prettyPrint();
+		patients.delPatient(first);
+		patients.prettyPrint();
+		patients.delPatient(last);
+		patients.prettyPrint();
     }
 
 }
