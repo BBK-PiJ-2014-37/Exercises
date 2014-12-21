cdcc4014689728fa9115835cf3b239db50c8008a
Joel Armando <joelarma@gmail.com>
2014-12-21 20:20:26 +0000

    Add (probably unfinished) solutions to exercises from day 4 & 5

---
+++ day4/binarytohexadecimal.groovy
@@ -0,0 +1,12 @@
+int getDecimal(String str) {
+	int d = (int)s.charAt(i) - (int)"0"
+    char c = s.charAt(i).toLower()
+    if (isDigit(c)) {
+        d = (int)c - (int)"0"
+    } else if (c >= "a" && c <= "f") {
+    	d = 10 + (int)c - (int)"a"
+	}
+d = n % b
+n = n / b
+}
+
---
+++ day4/decimal2hexadecimal.groovy
@@ -0,0 +1,20 @@
+int getDecimal(String str) {
+	int i = str.length()-1
+    while (i >= 0 ) {
+    	int d = (int)s.charAt(i) - (int)"0"
+       	char c = s.charAt(i).toLower()
+    	if (isDigit(c)) {
+        	d = (int)c - (int)"0"
+    	} else if (c >= "a" && c <= "f") {
+    		d = 10 + (int)c - (int)"a"
+		}
+		d = n % b
+		n = n / b
+		i--
+	}
+}
+
+
+
+
+
