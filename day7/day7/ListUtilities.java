c6103fb1ee84353c453b921eea7e11036238fde4
Joel Armando <joelarma@gmail.com>
2014-12-22 10:28:05 +0000

    Add bubbleSort method (Exercise 11)

--- day7/day7/ListUtilities.java
+++ day7/day7/ListUtilities.java
@@ -1,22 +1,47 @@
 package day7;
+
 public class ListUtilities {
 
-    private static IntegerList list;
-
-    public static IntegerList arrayToList(int[] ar) {
-	  int i = 0;
-	  list = new IntegerList();
-	  do {
-		list.add(ar[i]);
-		i++;
-		} while (i <  ar.length);
+	public static IntegerList arrayToList(int[] ar) {
+		int i = 0;
+		IntegerList list = new IntegerList();
+		do {
+			list.add(ar[i]);
+			i++;
+		} while (i < ar.length);
 		return list;
 	}
-	
-	public static void main (String[] args){
-    	int[] ls = {1,2,3,4,5,6,7,8,9,10};
-    	list = arrayToList(ls);
-    	list.prettyPrint();
+
+	public static void swap(IntegerNode i, IntegerNode j) {
+		IntegerNode temp = i;
+		i.setNumber(j.getNumber());
+		j.setNumber(temp.getNumber());
 	}
+	
+	public static void bubbleSort(IntegerList list) {
+		IntegerNode node = list.getFirst();
+		int c = 0;
+		while (node.getNext() != null) {
+			if (node.getNumber() > node.getNext().getNumber()) {
+				swap(node, node.getNext());
+			}
+			c++;
+		}
+		while (c > 1) {
+			for (int i = 0; i < c; i++);
+			if (node.getNumber() > node.getNext().getNumber()) {
+				swap(node, node.getNext());
+			}
+			c--;
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] ls = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+		IntegerList list = arrayToList(ls);
+		list.prettyPrint();
+		ListUtilities.bubbleSort(list);
+		list.prettyPrint();
+	}
+
 }
- 
