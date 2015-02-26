eadbfbc0e20ba3256b61aea84cd80b0fa8e3dd2d
Joel Armando <joelarma@gmail.com>
2015-02-26 12:27:14 +0000

    Complete exercises day20

--- day20/src/day20/ArrayChanger.java
+++ day20/src/day20/ArrayChanger.java
@@ -3,7 +3,7 @@ package day20;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Stream;
+
 
 public class ArrayChanger {
 
@@ -43,9 +43,30 @@ public class ArrayChanger {
 				.reduce("",(s1, s2) -> s1 + s2);
 		System.out.println(oneUpperCase1);
 		//9
-		Optional <String> oneWithCommas = words.stream().reduce((s1,s2) -> s1 + "," + s2);
+		Optional<String> oneWithCommas = words.stream().reduce(
+				(s1, s2) -> s1 + "," + s2);
 		System.out.println(oneWithCommas);
+		// 10
+		System.out.printf("8 random nums: %s.%n",
+				StreamUtils.randomNumberList(8));
+		// 11
+		System.out.printf("10 numbers starting at 50, by 5's: %s.%n",
+				StreamUtils.orderedNumberList(50, 5, 10));
+		//12
+		List<Integer> nums1 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+		int sum = nums1.stream().reduce(0,(x,y) -> x + y);
+		System.out.println("Sum: " + sum);
+		//13
+		int parallelSum = nums1.parallelStream().reduce(0,(x, y) -> x + y);
+		System.out.println("Parallel sum: " + parallelSum);
+		//14
+		List<Double> doub1 = Arrays.asList(1.02, 2.43, 3.0, 4.9, 5.0, 6.43, 7.9, 8.0);
+		Double prod = doub1.stream().reduce(1.0,(x,y) -> x * y);
+		System.out.println("Prod: " + prod);
 		
+		Double parallelProd = doub1.parallelStream().reduce(1.0,(x, y) -> x * y);
+		System.out.println("Parallel prod: " + parallelProd);
+				
 	}
 
 		
