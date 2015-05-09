95db4b6552cadc6c07089a1b644469ff6f88b74f
Joel Armando <joelarma@gmail.com>
2015-05-09 11:32:20 +0100

    Add solution exercise Anagram

--- day14/day14/AnagramRec.java
+++ day14/day14/AnagramRec.java
@@ -1,18 +1,36 @@
-package day14;
-public class Anagram {
-	public List<String> combinator(String str) {
-		if (str.size() == 0) {
-			return ArrayList<String>();
+import java.util.ArrayList;
+
+public class AnagramRec {
+	private String word;
+
+	public AnagramRec(String word){
+		this.word = word;
+	}
+
+	public ArrayList<String> combinator() {
+		ArrayList<String> combinations = new ArrayList<String>();
+ 		
+		if (word.length() == 0) {
+			combinations.add(this.word);
+			return combinations;
 		}
-		result = new ArrayList<String>();
-		for (int i = 0; i < str.size(); i++) {
-			char thisChar = str.charAt(i);
-			String rest = ""; /* str minus the char */
-			List<String> subAnagram = combinator(rest);
-			for (int j = 0; j < subAnagram.size()) {
-				result.add(thisChar.toString() + subAnagram.get(j));
+
+		for (int i = 0; i < word.length(); i++) {
+			String rest = "" + word.substring(0, i-1) + word.substring(i+1, word.length() -1); 
+			AnagramRec subAnagram = new AnagramRec(rest);
+			ArrayList<String> shortercombinator = subAnagram.combinator();
+			for (String s: shortercombinator) {
+				combinations.add(word.charAt(i) + s);
 			}
 		}
+		return combinations;
 	}
+
+	public static void main (String[] args) {
+		AnagramRec myAnagram = new AnagramRec("eat");
+		System.out.println(myAnagram.combinator().toString());
+
+	}
+
 
 }
